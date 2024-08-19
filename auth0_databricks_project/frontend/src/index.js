68cf06b95f65ba181691537fe744b8f0f0579e02
Italo Xesteres <31815779+Ixester@users.noreply.github.com>
2024-08-19 20:41:24 -0300

    Create index.js

---
+++ auth0_databricks_project/frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{ redirect_uri: window.location.origin, audience }}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
